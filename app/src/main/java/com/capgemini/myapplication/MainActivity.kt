a1c08fddbde9656c02c0aee86399ff0ecd3517f9
JCMedi102 <carlos.medj@gmail.com>
2024-05-05 21:36:24 -0600

    Create note list on home screen

--- app/src/main/java/com/capgemini/myapplication/MainActivity.kt
+++ app/src/main/java/com/capgemini/myapplication/MainActivity.kt
@@ -4,26 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.capgemini.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val notes = (1..100).map { "Note $it" }
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                LazyColumn {
+                    items(notes) {
+                        Text(
+                            text = it,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
                 }
             }
         }
